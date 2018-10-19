1091b112853a1c2aa61fb8dd8e3b45e0ba386546
Paul den Hertog <paul@den-hertog.net>
2018-10-19 14:50:25 +0200

    update

--- ovapi.py
+++ ovapi.py
@@ -1,21 +1,23 @@
 from datetime import datetime, timedelta
-import voluptuous as vol
-import http.client
 import logging, operator, json
+import http.client
 
+import voluptuous as vol
 
-from homeassistant.helpers.aiohttp_client import async_get_clientsession
 from homeassistant.components.sensor import PLATFORM_SCHEMA
+from homeassistant.const import (CONF_NAME)
+from homeassistant.helpers.aiohttp_client import async_get_clientsession
 from homeassistant.exceptions import PlatformNotReady
 from homeassistant.helpers.entity import Entity
 from homeassistant.util import Throttle
-from homeassistant.const import (
-    CONF_NAME, CONF_STOP_CODE, CONF_ROUTE_CODE)
 
 import homeassistant.helpers.config_validation as cv
 
 _LOGGER = logging.getLogger(__name__)
 _RESOURCE = 'v0.ovapi.nl'
+
+CONF_STOP_CODE = 'stop_code'
+CONF_ROUTE_CODE = 'route_code'
 
 ATTR_CREDITS = 'Data provided by v0.ovapi.nl'
 
@@ -38,11 +40,11 @@ async def async_setup_platform(
 
     session = async_get_clientsession(hass)
 
-    ovapi = OvApiData(stop_area_code)
+    ovapi = OvApiData(stop_code)
 
     await ovapi.async_update()
 
-    if ovapi.data is None:
+    if ovapi is None:
         raise PlatformNotReady
 
     sensors = [OvApiSensor(ovapi, name, stop_code, route_code)]
@@ -52,7 +54,7 @@ async def async_setup_platform(
 
 class OvApiSensor(Entity):
     def __init__(self, ovapi, name, stop_code, route_code):
-        self.data = ovapi
+        self.data = json.loads(ovapi.read())
         self._name = name
         self._stop_code = stop_code
         self._route_code = route_code
@@ -79,10 +81,10 @@ class OvApiSensor(Entity):
 
 
 class OvApiData:
-    def __init__(self, stop_area_code):
+    def __init__(self, stop_code):
         self.resource = _RESOURCE
-        self.stop_area_code = stop_area_code
-        self.data = {}
+        self.stop_code = stop_code
+        self.result = None
         self.headers = {
             'cache-control': "no-cache",
             'accept': "application/json"
@@ -90,13 +92,10 @@ class OvApiData:
 
     @Throttle(MIN_TIME_BETWEEN_UPDATES)
     async def async_update(self):
-
         try:
             response = http.client.HTTPConnection(self.resource)
-            response.request("GET", "/stopareacode/" + self.stop_area_code, headers = self.headers)
-            # await result = response.getresponse()
-            result = response.getresponse()
-            self.data = json.loads(result.read())
+            response.request("GET", "/stopareacode/" + self.stop_code, headers = self.headers)
+            self.result = response.getresponse()
             self.success = True
         except:
             _LOGGER.error("Impossible to get data from OvApi")
