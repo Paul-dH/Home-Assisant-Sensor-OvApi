fe749fae3e58fb11dc83790c1ddb788055dcfef6
Gyosa3 <51777889+Gyosa3@users.noreply.github.com>
2023-06-04 23:45:42 +0200

    Update __init__.py

---
+++ custom_components/ovapi/__init__.py
@@ -0,0 +1,38 @@
+from __future__ import annotations
+
+import logging
+
+import voluptuous as vol
+from homeassistant.const import SERVICE_RELOAD
+from homeassistant.core import HomeAssistant, ServiceCall
+from homeassistant.helpers.entity_component import EntityComponent
+from homeassistant.helpers.reload import async_reload_integration_platforms
+from homeassistant.helpers.typing import ConfigType
+
+from .const import DOMAIN, PLATFORMS, STARTUP_MESSAGE
+
+_LOGGER = logging.getLogger(__name__)
+
+
+async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
+    """Set up the platforms."""
+    # Print startup message
+    _LOGGER.info("ovapi component is starting...")
+
+    # await async_setup_reload_service(hass, DOMAIN, PLATFORMS)
+
+    component = EntityComponent(_LOGGER, DOMAIN, hass)
+
+    async def reload_service_handler(service: ServiceCall) -> None:
+        """Reload all sensors from config."""
+        print("+++++++++++++++++++++++++")
+        print(component)
+        # print(hass.data[DATA_INSTANCES]["sensors"].entities[0])
+
+        await async_reload_integration_platforms(hass, DOMAIN, PLATFORMS)
+
+    hass.services.async_register(
+        DOMAIN, SERVICE_RELOAD, reload_service_handler, schema=vol.Schema({})
+    )
+
+    return True
